d3c5b61c826b1c6b7560e93cdcc81e8157f1a6f1
AnkitSingh10-hub <59360110+AnkitSingh10-hub@users.noreply.github.com>
2020-01-14 16:51:58 +0545

    Add files via upload

---
+++ numpy array.py
@@ -0,0 +1,44 @@
+import numpy as np                                                                                                                           
+
+'''
+#arrays with numpy importation
+z=np.array([11,12,13])
+print(z)
+a=np.array([1,2,3])
+print(a)
+b=np.array([(1,2,3),(2,4,5)])
+print(b)
+
+
+'''
+
+'''
+#some functions to manipulate and know about arrays
+a=np.array([(1,2,3),(4,5,6)]
+print(a.ndim)
+print(a.itemsize)
+print(a.dtype)
+print(a.size)
+print(a.shape)
+'''
+'''
+#ways to display individual arrays
+
+a=np.array([(1,2,3,4),(3,4,5,6)])
+print(a[0,2])
+print(a[0],[2])
+print(a[:1,3])
+'''
+'''
+#some functions to manipulate the arrays with mathematics
+b=np.array([(1,2,3,4),(3,4,5,6),(7,8,9,10)])
+print(np.sqrt(a))
+print(np.std(a))
+print(np.ravel(a))
+
+
+'''
+matrix1=np.array
+
+
+           
